923bd22c27472d7df15feab0f29207a16bbd4adf
BaLiKfromUA <yuhimenko.valentin@gmail.com>
2020-05-24 00:38:53 +0300

    test todolist screen

--- lib/providers/todo_provider.dart
+++ lib/providers/todo_provider.dart
@@ -2,10 +2,63 @@ import 'package:flutter/material.dart';
 import '../models/todo.dart';
 
 class TodoProvider with ChangeNotifier {
-  List<Todo> _todoList = [];
+  List<Todo> _todoList = [
+    Todo(
+      id: 1,
+      timestamp: DateTime.now().millisecondsSinceEpoch,
+      label: "Work",
+      description: "Test",
+    ),
+    Todo(
+      id: 2,
+      timestamp: DateTime.now().millisecondsSinceEpoch,
+      label: "Work",
+      description: "Test 1",
+    ),
+    Todo(
+      id: 3,
+      timestamp: DateTime.now().millisecondsSinceEpoch,
+      label: "Work",
+      description: "Test 3",
+    ),
+    Todo(
+      id: 4,
+      timestamp: DateTime.now().millisecondsSinceEpoch,
+      label: "Work",
+      description: "Test 4",
+    ),
+  ];
+
+  DateTime _currentDay = DateTime.now();
+
+  DateTime get currentDay => _currentDay;
+
+  List<Todo> get currentDayTodolist => [..._todoList];
+
+  void next() {
+    _currentDay = _currentDay.add(
+      Duration(
+        days: 1,
+      ),
+    );
+  }
+
+  void prev() {
+    _currentDay = _currentDay.subtract(
+      Duration(
+        days: 1,
+      ),
+    );
+  }
+
+  Future<void> fetchTodolist() async {
+    return Future.delayed(const Duration(seconds: 1));
+    //TODO: api call and error handling
+  }
 
   void clear() {
     // TODO: api call and error handling
     _todoList.clear();
+    notifyListeners();
   }
 }
--- lib/screens/day_todolist_screen.dart
+++ lib/screens/day_todolist_screen.dart
@@ -1,8 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:intl/intl.dart';
+import '../providers/todo_provider.dart';
 import '../components/app_drawer.dart';
 
-class DayTodolistScreen extends StatelessWidget {
+class DayTodolistScreen extends StatefulWidget {
   static const String routeName = '/list';
+
+  @override
+  _DayTodolistScreenState createState() => _DayTodolistScreenState();
+}
+
+class _DayTodolistScreenState extends State<DayTodolistScreen> {
+  bool _isInit = true;
+  bool _isLoading = false;
+
+  void _fetchData() {
+    setState(() {
+      _isLoading = true;
+    });
+    Provider.of<TodoProvider>(context).fetchTodolist().then((_) {
+      setState(() {
+        _isLoading = false;
+      });
+    });
+  }
+
+  @override
+  void didChangeDependencies() {
+    if (_isInit) {
+      _fetchData();
+    }
+    _isInit = false;
+    super.didChangeDependencies();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,16 +41,79 @@ class DayTodolistScreen extends StatelessWidget {
 
     return Scaffold(
       appBar: appBar,
-      body: _buildTodoList(),
+      body: _isLoading
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : _buildTodoList(),
       drawer: AppDrawer(),
     );
   }
 
   Widget _buildNavBar() {
     return AppBar(
-      title: const Text('TODO app'),
+      title: _buildCurrentDayRow(),
+      actions: <Widget>[
+        IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.calendar_today),
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.add),
+        ),
+      ],
     );
   }
 
-  Widget _buildTodoList() {}
+  Widget _buildTodoList() {
+    MaterialLocalizations localizations = MaterialLocalizations.of(context);
+    final provider = Provider.of<TodoProvider>(context);
+    final todolist = provider.currentDayTodolist;
+
+    return ListView.builder(
+      itemCount: todolist.length,
+      itemBuilder: (BuildContext ctxt, int index) {
+        final currTask = todolist[index];
+        final taskTime = localizations.formatTimeOfDay(
+            TimeOfDay.fromDateTime(
+              DateTime.fromMillisecondsSinceEpoch(
+                currTask.timestamp,
+              ),
+            ),
+            alwaysUse24HourFormat: false);
+
+        return ListTile(
+          leading: Icon(Icons.event_available),
+          title: Text(currTask.description),
+          subtitle: Text("Time: $taskTime Tag: ${currTask.label}"),
+        );
+      },
+    );
+  }
+
+  Widget _buildCurrentDayRow() {
+    final provider = Provider.of<TodoProvider>(context);
+    final currDayStr =
+        DateFormat.yMMMMd("en_US").format(provider.currentDay).toString();
+
+    return Row(
+      children: <Widget>[
+        IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () {
+              provider.prev();
+              _fetchData();
+            }),
+        Text(currDayStr),
+        IconButton(
+          icon: Icon(Icons.arrow_forward),
+          onPressed: () {
+            provider.next();
+            _fetchData();
+          },
+        ),
+      ],
+    );
+  }
 }
